1dcd40ef015636011365a0e6a11a38c2753ec850
developeratdaguanyuan <developeratdaguanyuan@gmail.com>
2016-10-05 23:20:32 -0500

    Added entity_id_map and load data to mongoDB

--- MongoDB/src/main/java/Doraemon/MongoDB/UploadData.java
+++ MongoDB/src/main/java/Doraemon/MongoDB/UploadData.java
@@ -1,7 +1,12 @@
 package Doraemon.MongoDB;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.util.ArrayList;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -23,16 +28,24 @@ public class UploadData {
   public static String ENTITY_MAP = "./data/FB5M-entity-id.txt";
   public static String RELATION_MAP = "./data/FB5M-relation-id.txt";
   
-  public static Map<String, Integer> entity_id_map = new HashMap<String, Integer>();
-  public static Map<String, Integer> relation_id_map = new HashMap<String, Integer>();
+  public static Map<Integer, String> entity_id_map = new HashMap<Integer, String>();
+  public static Map<Integer, String> relation_id_map = new HashMap<Integer, String>();
   
-  public static void loadDict(String path, Map<String, Integer> id_map) {
-
-    
+  public static void loadDict(String path, Map<Integer, String> id_map) throws IOException {
+    File fin = new File(path);
+    BufferedReader reader = new BufferedReader(new FileReader(fin));
+    String line = null;
+    while ((line = reader.readLine()) != null) {
+      String[] tokens = line.trim().split("\t");
+      String id_m = tokens[0].trim();
+      String id_d = tokens[1].trim();
+      id_map.put(Integer.valueOf(id_d), id_m);
+    }
+    reader.close();
   }
   
-  
-  public static void updateFreebaseEmbedding(String db_name, String ct_name) {
+  public static void updateFreebaseEmbedding(
+      String db_name, String ct_name, String embed_file_path) {
     try {
       MongoClient mongoClient = new MongoClient("localhost", 27017);
 
@@ -42,24 +55,30 @@ public class UploadData {
         mongoDatabase.createCollection(ct_name);
         collection = mongoDatabase.getCollection(ct_name);
       }
-      List<Integer> array = new LinkedList<Integer>();
-      array.add(0);
-      array.add(1);
-      array.add(2);
-      Document document = new Document("word", "MongoDB");
-      document.append("array", array);
-      List<Document> documents = new ArrayList<Document>();  
-      documents.add(document);  
-      collection.insertMany(documents);
-      System.out.println("Connect to database successfully");
 
+      File file = new File(embed_file_path);
+      FileInputStream fin = new FileInputStream(file);
+      BufferedInputStream bin = new BufferedInputStream(fin);
+      DataInputStream din = new DataInputStream(bin);
+      int count = (int) (file.length() / 4);
+      for (int i = 0; i < count / 256; i++) {
+        System.out.println(i);
+        List<Double> array = new LinkedList<Double>();
+        for (int j = 0; j < 256; j++) {
+          array.add((double)din.readFloat());
+        }
+        Document document = new Document("mid", entity_id_map.get(i + 1));
+        document.append("id", i + 1);
+        document.append("embedding", array);
+        collection.insertOne(document);
+      }
     } catch (Exception e) {
       System.err.println(e.getClass().getName() + ": " + e.getMessage());
     }
   }
   
-  public static void main(String[] args) {
-    updateFreebaseEmbedding("test", "collection");
-    
+  public static void main(String[] args) throws IOException {
+    loadDict(ENTITY_MAP, entity_id_map);
+    updateFreebaseEmbedding("test", "collection", ENTITY_EMBEDDING);
   }
 }
